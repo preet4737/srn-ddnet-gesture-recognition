d60521604bdb2905eb3b26bb21d8a37bbc72f02d
Vikrant Gajria <vikrantgajria@gmail.com>
2021-03-29 17:41:41 +0530

    Shorten SRN code and update Notebook with visualization

--- srn/srn.py
+++ srn/srn.py
@@ -36,18 +36,17 @@ class HandPose(object):
         self.G = multi_stage(config)
 
         self.draw_dir = config.draw_dir
-
+        
     def run(self, data_dir):
-        poses_xyz = list()
-        poses_uvd = list()
-        poses_world = list()
-        paras = (440.44232, 461.0357, -0.00015258789, 3.0517578e-05)
-#         self.testData = dhg.realtime_loader(data_dir, (615.866, 615.866, 316.584, 228.38),
-#                                         cube_size=self.cube_size) 
-#         self.testData = dhg.realtime_loader(data_dir, (440.478, 461.056, 316.584, 228.38),
-#                                         cube_size=self.cube_size) 
-        self.testData = dhg.realtime_loader(data_dir, (440.44232, 461.0357, -0.00015258789, 3.0517578e-05),
-                                        cube_size=self.cube_size) 
+        poses_image = list() # To store image coordinates
+        poses_world = list() # To store DHG's world coordinates
+        
+        # paras = (615.866, 615.866, 316.584, 228.38) # Copied from realsense dataset
+        paras = (440.44232, 461.0357, -0.00015258789, 3.0517578e-05) # 
+    
+        hands172dhg = [0, 1, 6, 7, 8, 2, 9, 10, 11, 3, 12, 13, 14, 4, 15, 16, 17, 5, 18, 19, 20]
+
+        self.testData = dhg.realtime_loader(data_dir, paras, cube_size=self.cube_size) 
 
         self.testLoader = DataLoader(self.testData, batch_size=1, shuffle=False, num_workers=1)
         self.G.load_state_dict(torch.load(os.path.join(self.model_dir), map_location=lambda storage, loc: storage))
@@ -61,39 +60,30 @@ class HandPose(object):
                 outputs, features = self.G(img, self.GFM_, self.testData, M, cube, center)
                 for index in range(0, len(outputs), 3):
                     output = outputs[index][0].view(1, -1, 3)
+                    
                     joints_xyz = self.testData.uvd_nl2xyznl_tensor(output, M, cube, center)
                     
-#                     center[0][0] *= -1
-#                     center[0][0] += 320
-#                     center[0][1] += 240
-                    
-#                     cube[0][0] *= -1
-                    
-                    hands172dhg = [0, 1, 6, 7, 8, 2, 9, 10, 11, 3, 12, 13, 14, 4, 15, 16, 17, 5, 18, 19, 20]
-                    joints_xyz = \
-                    (joints_xyz * cube.view(1, 1, 3) / 2 + center.view(1, 1, 3))
+                    # Transform out of cube
+                    joints_xyz = joints_xyz * cube.view(1, 1, 3) / 2 + center.view(1, 1, 3)
             
-                    joints_xyz_np = joints_xyz.cpu().numpy()[0][hands172dhg, :]
-                    poses_xyz.append(joints_xyz_np)
-                
                     # image coordinates
-                    joints_uvd = self.testData.joints3DToImg(joints_xyz).cpu().numpy()[0][hands172dhg, :]
-                    poses_uvd.append(joints_uvd)
+                    joints_image = self.testData.joints3DToImg(joints_xyz).cpu().numpy()[0][hands172dhg, :]
                     
-                    # world coordinates
-                    joints_world = joints_uvd.copy()
+                    # DHG's world coordinates
+                    joints_world = joints_image.copy()
                     joints_world[:, 2] = joints_world[:, 2] * 0.00093 
                     joints_world[:, 0] = ((640 - joints_world[:, 0]) - paras[2]) * joints_world[:, 2] / paras[0]
                     joints_world[:, 1] = ((joints_world[:, 1]) - paras[3]) * joints_world[:, 2] / -paras[1]
                     poses_world.append(joints_world)
 
                     # Pose Image generation
-                    img_draw = (img.cpu().numpy() + 1) / 2 * 255
-                    joints_draw = (output.detach().cpu().numpy() + 1) * (self.input_size / 2)
-                    img_dir =  self.draw_dir + str(batch_idx) +  '_depth.png'
-                    img_show = draw_pose("hands17", cv2.cvtColor(img_draw[0, 0], cv2.COLOR_GRAY2RGB),
-                                         joints_draw[0])
-                    cv2.imwrite(img_dir, img_show)
+                    if False:
+                        img_draw = (img.cpu().numpy() + 1) / 2 * 255
+                        joints_draw = (output.detach().cpu().numpy() + 1) * (self.input_size / 2)
+                        img_dir =  self.draw_dir + str(batch_idx) +  '_depth.png'
+                        img_show = draw_pose("hands17", cv2.cvtColor(img_draw[0, 0], cv2.COLOR_GRAY2RGB),
+                                             joints_draw[0])
+                        cv2.imwrite(img_dir, img_show)
 
         # return world coordinates
         return np.array(poses_world)
