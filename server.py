77a11b06ea73ec02dd915ee312aab0641a981a47
Vikrant Gajria <vikrantgajria@gmail.com>
2021-03-31 16:46:17 +0530

    Make index files instead of pickle for frontend

--- server.py
+++ server.py
@@ -24,28 +24,6 @@ app.url_map.strict_slashes = False
 api = Api(app)
 #
 
-#######################
-# Dataset Information #
-
-with open( C['dataset']['index_file'], 'rb' ) as f:
-    index = pickle.load( f )
-
-#
-
-@app.route('/info/<g>')
-def get_essais( g: str ) -> list:
-    """ Returns essai list for a gesture """
-    essais = index[g]['essais']
-    essais = jsonify(essais)
-    return essais
-
-@app.route('/info/<g>/<f>/<s>/<e>')
-def get_pcds( g: str, f: str, s: str, e: str ) -> list:
-    """ Returns PCD list for an essai """
-    pcds = index[g][f][s][e]
-    pcds = jsonify(pcds)
-    return pcds
-
 ####################
 # Machine Learning #
 
